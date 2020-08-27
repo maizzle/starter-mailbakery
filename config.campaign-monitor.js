fa3b88882ad62d9eb7edf45cbeae1912c48b1e5b
Cosmin Popovici <hellocosmin@gmail.com>
2020-08-27 20:08:40 +0300

    refactor: make compatible with maizzle v1.x

--- config.campaign-monitor.js
+++ config.campaign-monitor.js
@@ -3,26 +3,17 @@ module.exports = {
     destination: {
       path: 'dist/campaign-monitor',
     },
-    layout: 'src/layouts/default.njk',
     templates: {
-      source: 'src/templates/campaign-monitor',
+      root: 'src/templates/campaign-monitor',
     },
   },
-
   inlineCSS: {
     enabled: true,
   },
-
-  cleanup: {
-    removeUnusedCSS: {
-      enabled: true,
-    },
-    replaceStrings: {
-      '\\s(style|class)(=""|(?=>)|(?=\\s))+': '', // remove empty style or class attributes
-    },
-  },
-
   prettify: {
+    enabled: true,
+  },
+  removeUnusedCSS: {
     enabled: true,
   },
 }
